c8df7a0d57bf9feaa6597d8544868833131e4ba7
Студенческая <lunwyze@gmail.com>
2025-05-23 13:48:18 +0500

    o_O

--- js/script.js
+++ js/script.js
@@ -5,3 +5,28 @@ b = 3;
 sum = a*b;
 console.log(sum);
 }
+
+{
+//let photofolder,photopage,numberpage;
+//photofolder = parseInt(prompt("Vvedtie kol-vo photo v papke", 10))
+//photopage = parseInt(prompt("Vvedtie kol-vo photo v papke", 10))
+//numberpage = Math.ceil(photofolder / photopage)
+//alert("kolvo stranits = " + numberpage);
+} 
+
+{
+let a = 14 % 5;
+let b = Math.floor(14 % 5);
+console.log(a,b);
+
+
+
+let number,d1,d2,d3,d4;
+number = parseInt(prompt("vvod"))
+d1 = Math.floor(number/1) % 10;  
+d2 = Math.floor(number/10) % 10;
+d3 = Math.floor(number/100) % 10;
+d4 = Math.floor(number/1000) % 10
+console.log(d1,d2,d3,d4)
+}
+
